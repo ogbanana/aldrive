35b3dc5af93d71e721aac3a29acf084d2d99ce0e
Amy Liao <a.liao.uey@gmail.com>
2021-02-04 21:35:10 -0500

    viewport and opengraph set up

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,11 @@ const index = () => {
       <Head>
         <title>Amy Liao</title>
         <link rel="icon" type="images/png" href="/images/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="Amy Liao" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://aldrive.vercel.app/" />
+        <meta property="og:image" content="../images/share.png" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Spartan:wght@100;200;300&display=swap"
