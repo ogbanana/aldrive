42ae801e96b99dc9d15b6cb5f17140b510022a22
Amy Liao <a.liao.uey@gmail.com>
2021-03-15 20:33:46 -0400

    Add hover text to nav items. Add tech stack under projects. Reorganize projects.

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 
 const Nav = () => {
   return (
     <>
-      <ul className="flex flex-row justify-center md:justify-end w-full h-14 text-2xl pt-2 lg:pr-1 mb-8">
+      <ul className="flex flex-row justify-center md:justify-end w-full h-14 text-2xl pt-5 lg:pr-10 mb-8">
         <li className="navItem">
           <a title="Email" href="mailTo:amy.g.liao@gmail.com" target="_blank">
-            <img src="../images/email.png" className="h-8" />
+            <img src="../images/email.png" className="h-9" />
+            <span className="toolTip">Email</span>
           </a>
         </li>
         <li className="navItem">
@@ -16,17 +17,20 @@ const Nav = () => {
             href="https://docs.google.com/document/d/12q33sKB0zyBlETuO2YaXeI3qU73SuboXC9a3G-uBUwk/edit?usp=sharing"
             target="_blank"
           >
-            <img src="../images/resume.png" className="h-8" />
+            <img src="../images/resume.png" className="h-10" />
+            <div className="toolTip">Resume</div>
           </a>
         </li>
         <li className="navItem">
           <a title="Github" href="https://github.com/ogbanana" target="_blank">
-            <img src="../images/GitHub-Mark-Light-64px.png" className="h-8" />
+            <img src="../images/GitHub-Mark-Light-64px.png" className="h-10" />
+            <div className="toolTip">Github</div>
           </a>
         </li>
         <li className="navItem">
           <a title="LinkedIn" href="https://www.linkedin.com/in/amyliao123/" target="_blank">
-            <img src="../images/LinkedIn.png" className="h-8" />
+            <img src="../images/LinkedIn.png" className="h-10" />
+            <div className="toolTip">LinkedIn</div>
           </a>
         </li>
       </ul>
--- components/TechSkills.tsx
+++ components/TechSkills.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import allTech from '../utils/tech.json'
 
 const TechSkills = () => {
-  return <>TechSkills coming...</>
+  return (
+    <div className="bg-black w-full flex flex-col items-center lg:pl-5 lg:pr-5 lg:pt-10 lg:pb-10">
+      <h1 className="text-xl lg:text-4xl pt-5 pb-15">Tech Stack</h1>
+      <div className="flex flex-wrap justify-around w-4/5 h-full">
+        {allTech.map((tech, index) => {
+          let key = tech + index.toString()
+          return (
+            <a key={key} className="p-1" href={tech.url} target="_blank" rel="noopener noreferrer">
+              <img className="techLogos" src={tech.path} />{' '}
+            </a>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default TechSkills
